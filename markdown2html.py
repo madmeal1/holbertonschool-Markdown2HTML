3f259e20157fb23e9770cbc0bc3a6df12fbee7d8
Elmael33 <mael.salet33@gmail.com>
2025-09-19 14:48:54 +0200

    [feat] implement markdown to html converter and a heading parser

--- markdown2html.py
+++ markdown2html.py
@@ -2,8 +2,9 @@
 """Converts a Markdown file to HTML file."""
 import sys
 import os
+import re
 
-"""Converts a Markdown file to HTML file."""
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
@@ -17,6 +18,13 @@ if __name__ == "__main__":
         print("Missing {}".format(input_file), file=sys.stderr)
         sys.exit(1)
 
-        pass
+    with open(input_file, "r") as md, open(output_file, "w") as html:
+        for line in md:
+            line = line.rstrip("\n")
+            match = re.match(r"^(#{1,6}) (.*)", line)
+            if match:
+                level = len(match.group(1))
+                content = match.group(2)
+                html.write("<h{}>{}</h{}>\n".format(level, content, level))
 
     sys.exit(0)
