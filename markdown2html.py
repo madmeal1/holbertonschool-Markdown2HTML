4d4ca7a8ad3572e5c5ab3a06bf9d475fa704e4f7
Elmael33 <mael.salet33@gmail.com>
2025-09-19 14:02:11 +0200

    [fix] test comment section and remove with open

--- markdown2html.py
+++ markdown2html.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Converts a Markdown file to HTML file."""
 import sys
 import os
 
+"""Converts a Markdown file to HTML file."""
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
@@ -14,7 +16,6 @@ if __name__ == "__main__":
         print("Missing {}".format(input_file), file=sys.stderr)
         sys.exit(1)
 
-    with open(output_file, 'w') as f:
         pass
 
     sys.exit(0)
